be2259ff60495ec0af6846f6be414064e985ac0e
dcsan <dc@pikkle.com>
2014-12-04 04:24:58 -0800

    added author namespace (me for now...)

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
+  name: 'dcsan:facebook-sdk',
   summary: "Facebook SDK packaged for Meteor",
-  version: "0.2.0",
-  git: "https://github.com/biasport/facebook-sdk.git"
+  version: "0.2.1",
+  git: "https://github.com/dcsan/facebook-sdk"
 });
 
 Package.on_use(function(api) {
-  api.versionsFrom("METEOR@0.9.0");
+  api.versionsFrom("METEOR@1.0");
     api.use(['jquery', 'spacebars', 'templating']);
     api.add_files(['sdk.js', 'templates.html'], 'client');
 });
